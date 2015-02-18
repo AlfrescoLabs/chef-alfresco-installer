b0060d8008403eafa600153078c21c1dd8d2f047
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2015-02-18 09:38:56 +0200

    modified berkshelf

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source "https://supermarket.chef.io"
 
 metadata
+
+cookbook "java-wrapper", "~> 0.1", path: "../java-wrapper"
--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,7 @@ maintainer_email 'sergiu.vidrascu@ness.com'
 license          'All rights reserved'
 description      'Installs/Configures alfresco-chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.0'
 
-depends "java-wrapper"
 depends "chef-client"
 depends "apt"
