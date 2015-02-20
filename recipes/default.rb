84f941d8e1b4e3d4e763dfd9767183e0db3f1540
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2015-02-20 12:07:39 +0200

    Fix fc006 issue
    Modified file permissions

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,7 +12,9 @@ creds["pass"] # will be decrypted
 remote_file "/opt/alfresco.bin" do
    source "ftp://#{creds["bamboo_username"]}:#{creds["bamboo_passwprd"]}@ftp.alfresco.com/#{node["alfresco_download_path"]}"
    checksum '90c61a7d7e73c03d5bfeb78de995d1c4b11959208e927e258b4b9e74b8ecfffa'
-   mode 777
+   owner "root"
+   group "root"
+   mode "644"
    action :create_if_missing
 end
 
