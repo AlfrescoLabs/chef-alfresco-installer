ee75039d012e59e8a65593ac5588c368babe6f1a
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2015-02-20 16:30:13 +0200

    Modified Berksfiles
    Modified Berksfiles and added data_bags

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,11 +6,11 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-creds = Chef::EncryptedDataBagItem.load("bamboo", "pass", secret = "sergiu")
+creds = Chef::EncryptedDataBagItem.load("bamboo", "pass")
 creds["pass"] # will be decrypted
 
 remote_file "/opt/alfresco.bin" do
-   source "ftp://#{creds["bamboo_username"]}:#{creds["bamboo_passwprd"]}@ftp.alfresco.com/#{node["alfresco_download_path"]}"
+   source "ftp://#{creds["bamboo_username"]}:#{creds["bamboo_password"]}@ftp.alfresco.com/#{node["alfresco_download_path"]}"
    checksum '90c61a7d7e73c03d5bfeb78de995d1c4b11959208e927e258b4b9e74b8ecfffa'
    owner "root"
    group "root"
