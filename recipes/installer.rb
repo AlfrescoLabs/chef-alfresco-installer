8ff4c4caba8826f5298299514b7d5060b2106fc8
Sergiu Vidrascu <vsergiu@hotmail.com>
2015-03-03 00:01:45 +0200

    update installer location and kitchen yml

--- recipes/installer.rb
+++ recipes/installer.rb
@@ -10,7 +10,7 @@
 creds = Chef::EncryptedDataBagItem.load("bamboo", "pass")
 creds["pass"] # will be decrypted
 
-remote_file "/opt/alfresco.bin" do
+remote_file "/resources/alfresco.bin" do
    source "ftp://#{creds["bamboo_username"]}:#{creds["bamboo_password"]}@ftp.alfresco.com/#{node["alfresco_download_path"]}"
    checksum '90c61a7d7e73c03d5bfeb78de995d1c4b11959208e927e258b4b9e74b8ecfffa'
    owner "root"
@@ -21,7 +21,7 @@ remote_file "/opt/alfresco.bin" do
 end
 
 execute "install alfresco" do
-	command "/opt/alfresco.bin --mode unattended --alfresco_admin_password admin --enable-components alfrescowcmqs --disable-components javaalfresco,alfrescosolr --jdbc_username alfresco --jdbc_password alfresco --prefix /opt/target/alf-installation"
+	command "/resources/alfresco.bin --mode unattended --alfresco_admin_password admin --enable-components alfrescowcmqs --disable-components javaalfresco,alfrescosolr --jdbc_username alfresco --jdbc_password alfresco --prefix /opt/target/alf-installation"
 	creates '/opt/target/alf-installation/alfresco.sh'
 end
 
