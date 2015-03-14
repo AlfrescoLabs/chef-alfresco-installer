4370c2790f224009b32dfe40d534a59ab8adc5e4
Sergiu Vidrascu <vsergiu@hotmail.com>
2015-03-14 13:03:42 +0200

    Added copyright information

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,3 +10,7 @@ This file is used to list changes made in each version of the java-wrapper cookb
 0.2.0
 -----
 - Sergiu Vidrascu - Added java 7 custom location and also parameters for windows and solaris
+
+0.2.5
+-----
+- Sergiu Vidrascu - Modified recipe to work on Redhat 6.5, Windows 2012R2, Suse 12, UBUNTU 12.04
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'java-wrapper'
 maintainer       'Sergiu Vidrascu'
 maintainer_email 'sergiu.vidrascu@ness.com'
-license          'All rights reserved'
+license          '2005-2015 Alfresco Software Limited'
 description      'Installs/Configures alfresco_java'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.5'
 
 depends "java"
 depends "windows"
