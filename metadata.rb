95b8de43013790e36e46dbaf4193ee900e4d731c
Sergiu Vidrascu <vsergiu@hotmail.com>
2015-03-14 19:36:56 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'roles'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures roles'
+long_description 'Installs/Configures roles'
+
+version          '0.1.0'
+
