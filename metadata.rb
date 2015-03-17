e59062bb7e6ab28cc6a0d253c17d4d1b385ed2a5
Sergiu Vidrascu <sergiu.vidrascu@ness.com>
2015-03-17 21:14:47 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'alfresco-metal'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures alfresco-metal'
+long_description 'Installs/Configures alfresco-metal'
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: alfresco-metal
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
