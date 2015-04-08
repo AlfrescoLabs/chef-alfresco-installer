4f3a27dd2454dac579ba4b5a6ae728666cd16182
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2015-04-08 19:42:09 +0300

    modify serverspec tests

--- test/integration/installer/serverspec/installer_spec.rb
+++ test/integration/installer/serverspec/installer_spec.rb
@@ -30,20 +30,8 @@ describe port(50500) do
   it { should be_listening}
 end
 
-describe port(8100) do
-  it { should be_listening }
-end
-
 describe port(8443) do
   it { should be_listening }
-end
-
-describe process("postgres") do
-  it { should be_running }
-end
-
-describe process("tomcat") do
-  it { should be_running }
 end
 
 describe service('alfresco') do
