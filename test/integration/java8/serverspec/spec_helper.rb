82d5c303fc4b4a7834d49f1b8ac94e97d25458cc
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2015-04-28 13:24:36 +0300

    update test reporting

--- test/integration/java8/serverspec/spec_helper.rb
+++ test/integration/java8/serverspec/spec_helper.rb
@@ -1,12 +1,10 @@
 require 'busser/rubygems'
-Busser::RubyGems.install_gem('yarjuf', '~> 2.0.0')
-
 require 'serverspec'
 require 'yarjuf'
 
 set :backend, :exec
 
 RSpec.configure do |c|
-  c.output_stream = File.open('/resources/serverspec-result.xml', 'w')
-  c.formatter = 'JUnit'
+  c.output_stream = File.open('/opt/serverspec.html', 'w')
+  c.formatter = 'html'
 end
