cd2ca8ad11ec569cd27413a5faadbe684cda3e06
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2015-05-06 18:53:27 +0300

    modify loadbalancer

--- attributes/loadbalancer.rb
+++ attributes/loadbalancer.rb
@@ -1,1 +1,1 @@
-default["lb"]["ips_and_nodenames"] = [{:ip=>172.29.101.97, :nodename=>"alf1"},{:ip=>172.29.101.99, :nodename=>"alf2"}]
+default["lb"]["ips_and_nodenames"] = [{:ip=>"172.29.101.97", :nodename=>"alf1"},{:ip=>"172.29.101.99", :nodename=>"alf2"}]
