5e102adb0e4edd4d87ab73a8121dd614d4edbd80
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2015-05-25 12:34:14 +0300

    correct guard for alfresco installation

--- recipes/installer.rb
+++ recipes/installer.rb
@@ -190,7 +190,7 @@ case node['platform_family']
 
     execute 'Install alfresco' do
       command "#{node['installer']['local']} --mode unattended --alfresco_admin_password #{node['installer']['alfresco_admin_password']} --enable-components #{node['installer']['enable-components']} --disable-components #{node['installer']['disable-components']} --jdbc_username #{node['installer']['jdbc_username']} --jdbc_password #{node['installer']['jdbc_password']} --prefix #{node['installer']['directory']}"
-      not_if { File.exists?("#{node['installer']['directory']}//alfresco.sh") }
+      not_if { File.exists?("#{node['installer']['directory']}/alfresco.sh") }
     end
 
     template "#{node['installer']['directory']}/tomcat/shared/classes/alfresco-global.properties" do
