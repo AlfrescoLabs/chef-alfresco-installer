83508f7c2cb3331c9d51bf9c265171cda1c0a836
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2015-05-29 12:38:09 +0300

    corrected target host location

--- spec/checklist/checklist_spec.rb
+++ spec/checklist/checklist_spec.rb
@@ -2,14 +2,21 @@ require 'spec_helper'
 
 currentDir=Dir.pwd
 propertiesFile = {}
-IO.foreach("#{currentDir}/test.properties") do |line|
+IO.foreach("#{currentDir}/test.glProps") do |line|
   propertiesFile[$1.strip] = $2 if line =~ /([^=]*)=(.*)\/\/(.*)/ || line =~/([^=]*)=(.*)/
 end
-output = "File Name #{"#{currentDir}/test.properties"} \n"
+output = "File Name #{"#{currentDir}/test.glProps"} \n"
 propertiesFile.each {|key,value| output += " #{key}= #{value} \n" }
 
 
 puts "\n Running tests on: \n" + command('ifconfig | grep "inet .*"').stdout
+
+
+if ENV['checklist_target_host']
+  target_host = ENV['checklist_target_host']
+else
+  target_host = propertiesFile['host']
+end
 
 describe 'Alfresco Global Checks' do
 
@@ -18,7 +25,7 @@ describe 'Alfresco Global Checks' do
   it {expect(port(8443)).to be_listening}
   it {expect(service('alfresco')).to be_enabled}
 
-  connection = Faraday.new(:url => "http://#{propertiesFile['host']}:8080",
+  connection = Faraday.new(:url => "http://#{target_host}:8080",
                            :headers => {'Host' => host_inventory['hostname']}) do |faraday|
     faraday.adapter Faraday.default_adapter
   end
@@ -28,7 +35,7 @@ describe 'Alfresco Global Checks' do
   it {expect(connection.get('/alfresco/').status).to eq 200}
   it {expect(connection.get('/alfresco/').body).to include('Alfresco WebScripts Home')}
 
-  connection2 = Faraday.new(:url => "http://admin:admin@#{propertiesFile['host']}:8080",
+  connection2 = Faraday.new(:url => "http://admin:admin@#{target_host}:8080",
                             :headers => {'Host' => host_inventory['hostname']}) do |faraday|
     faraday.adapter Faraday.default_adapter
   end
@@ -45,28 +52,28 @@ describe 'Alfresco Global Checks' do
 
 end
 if ENV['checklist_target_alf_glob']
-  alfrescoGlobalLocation = ENV['checklist_target_alf_glob']
+  alfrescoGlobalFileLocation = ENV['checklist_target_alf_glob']
 else
-  alfrescoGlobalLocation = propertiesFile['alfrescoGlobalLocation']
+  alfrescoGlobalFileLocation = propertiesFile['alfrescoGlobalFileLocation']
 end
 
-String globalProperties = file(alfrescoGlobalLocation).content
-properties = {}
-globalProperties.each_line do |line|
-  properties[$1.strip] = $2 if line =~ /([^=]*)=(.*)\/\/(.*)/ || line =~/([^=]*)=(.*)/
+String globalPropertiesFile = file(alfrescoGlobalFileLocation).content
+glProps = {}
+globalPropertiesFile.each_line do |line|
+  glProps[$1.strip] = $2 if line =~ /([^=]*)=(.*)\/\/(.*)/ || line =~/([^=]*)=(.*)/
 end
-output = 'File Name alfresco-global.properties \n'
-properties.each {|key,value| output += " #{key}= #{value} \n" }
+output = 'File Name alfresco-global.glProps \n'
+glProps.each {|key,value| output += " #{key}= #{value} \n" }
 
 describe 'FTP/FTPS settings' do
-  it {expect(properties).to include('ftp.enabled' => 'true')}
-  it {expect(properties).not_to include('ftp.port' => '')}
-  context "and the port set : #{properties['ftp.port']}" do
-    it {expect(port(properties['ftp.port'])).to be_listening}
+  it {expect(glProps).to include('ftp.enabled' => 'true')}
+  it {expect(glProps).not_to include('ftp.port' => '')}
+  context "and the port set : #{glProps['ftp.port']}" do
+    it {expect(port(glProps['ftp.port'])).to be_listening}
   end
 end
 
 describe 'JBPM settings' do
-  it {expect(properties).to include('system.workflow.engine.jbpm.enabled' => 'true')}
-  it {expect(properties).to include('system.workflow.engine.jbpm.definitions.visible' => 'true')}
+  it {expect(glProps).to include('system.workflow.engine.jbpm.enabled' => 'true')}
+  it {expect(glProps).to include('system.workflow.engine.jbpm.definitions.visible' => 'true')}
 end
