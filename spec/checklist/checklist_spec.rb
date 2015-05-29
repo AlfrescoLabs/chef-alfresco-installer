513bff1be179fd4d98e35f18a8a3c4a5ee40df4b
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2015-05-29 11:22:37 +0300

    added {}

--- spec/checklist/checklist_spec.rb
+++ spec/checklist/checklist_spec.rb
@@ -13,35 +13,35 @@ puts host_inventory['hostname']
 
 describe 'Alfresco Global Checks' do
 
-  it expect(port(8080)).to be_listening
-  it expect(port(5432)).to be_listening
-  it expect(port(8443)).to be_listening
-  it expect(service('alfresco')).to be_enabled
+  it {expect(port(8080)).to be_listening}
+  it {expect(port(5432)).to be_listening}
+  it {expect(port(8443)).to be_listening}
+  it {expect(service('alfresco')).to be_enabled}
 
   connection = Faraday.new(:url => "http://#{propertiesFile['host']}:8080",
                            :headers => {'Host' => host_inventory['hostname']}) do |faraday|
     faraday.adapter Faraday.default_adapter
   end
 
-  it expect(connection.get('').body).to include('Welcome to Alfresco!')
-  it expect(connection.get('/share/page').status).to eq 200
-  it expect(connection.get('/alfresco/').status).to eq 200
-  it expect(connection.get('/alfresco/').body).to include('Alfresco WebScripts Home')
+  it {expect(connection.get('').body).to include('Welcome to Alfresco!')}
+  it {expect(connection.get('/share/page').status).to eq 200}
+  it {expect(connection.get('/alfresco/').status).to eq 200}
+  it {expect(connection.get('/alfresco/').body).to include('Alfresco WebScripts Home')}
 
   connection2 = Faraday.new(:url => "http://admin:admin@#{propertiesFile['host']}:8080",
                             :headers => {'Host' => host_inventory['hostname']}) do |faraday|
     faraday.adapter Faraday.default_adapter
   end
 
-  it expect(connection2.get('/alfresco/s/index').status).to eq 200
-  it expect(connection2.get('/alfresco/s/index').body).to include('Browse all Web Scripts')
-  it expect(connection2.get('/alfresco/webdav').status).to eq 200
-  it expect(connection2.get('/alfresco/webdav').body).to include('Data Dictionary')
-  it expect(connection2.get('/alfresco/webdav').body).to include('Directory listing for /')
-  it expect(connection2.get('/alfresco/s/enterprise/admin/admin-systemsummary').status).to eq 200
-  it expect(connection2.get('/alfresco/s/enterprise/admin/admin-systemsummary').body).to include('System Summary')
-  it expect(connection2.get('/alfresco/s/enterprise/admin/admin-systemsummary').body).to match(">Solr\ 4.*\n.*\n.*Enabled")
-  it expect(connection2.get('/alfresco/s/api/solrstats').status).to eq 200
+  it {expect(connection2.get('/alfresco/s/index').status).to eq 200}
+  it {expect(connection2.get('/alfresco/s/index').body).to include('Browse all Web Scripts')}
+  it {expect(connection2.get('/alfresco/webdav').status).to eq 200}
+  it {expect(connection2.get('/alfresco/webdav').body).to include('Data Dictionary')}
+  it {expect(connection2.get('/alfresco/webdav').body).to include('Directory listing for /')}
+  it {expect(connection2.get('/alfresco/s/enterprise/admin/admin-systemsummary').status).to eq 200}
+  it {expect(connection2.get('/alfresco/s/enterprise/admin/admin-systemsummary').body).to include('System Summary')}
+  it {expect(connection2.get('/alfresco/s/enterprise/admin/admin-systemsummary').body).to match(">Solr\ 4.*\n.*\n.*Enabled")}
+  it {expect(connection2.get('/alfresco/s/api/solrstats').status).to eq 200}
 
 end
 
