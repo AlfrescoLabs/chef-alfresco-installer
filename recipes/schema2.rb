23f3c9ac6245ef6885b43c693840efc0a6dda501
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2015-11-09 17:49:01 +0200

    Updated transport options on schema 2

--- recipes/schema2.rb
+++ recipes/schema2.rb
@@ -12,25 +12,43 @@ with_driver 'ssh'
 # with_chef_server "http://172.29.101.100:4000"
 with_chef_local_server :chef_repo_path => '/tmp/kitchen/cache', :cookbook_path => '/tmp/kitchen/cache/cookbooks'
 
+clusternode1=node['node1']
+clusternode2=node['node2']
+loadbalancer=node['loadbalancer']
+username='root'
+installerPath='ftp://172.29.101.56/51/b295/alfresco-enterprise-installer-20150904-SNAPSHOT-295-linux-x64.bin'
+
+node1_options = {:transport_options => {
+                    :ip_address => clusternode1,
+                    :username => username,
+                    :ssh_options => {
+                        :password => 'alfresco'
+                    }
+                }}
+node2_options = {:transport_options => {
+                    :ip_address => clusternode2,
+                    :username => username,
+                    :ssh_options => {
+                        :password => 'alfresco'
+                    }
+                }}
+lb_options = {:transport_options => {
+                    :ip_address => loadbalancer,
+                    :username => username,
+                    :ssh_options => {
+                        :password => 'alfresco'
+                    }
+                }}
+
+
+
 machine_batch 'Initial setup on nodes and lb' do
 
-  clusternode1=node['node1']
-  clusternode2=node['node2']
-  loadbalancer=node['loadbalancer']
-  username='root'
-
-  installerPath='ftp://172.29.101.56/51/b295/alfresco-enterprise-installer-20150904-SNAPSHOT-295-linux-x64.bin'
 
   machine 'node1' do
       action [:ready, :setup, :converge]
-      machine_options :transport_options => {
-                          :ip_address => clusternode1,
-                          :username => username,
-                          :ssh_options => {
-                              :password => 'alfresco'
-                          }
-                      }
-      run_list %w(recipe[java-wrapper::java8] recipe[alfresco-installer::installer])
+      machine_options node1_options
+      run_list %w(recipe[java-wrapper::java8] recipe[nfs::client4] recipe[alfresco-installer::installer])
       attributes 'installer' =>
                      {'nodename' => 'node1',
                       'disable-components' => 'javaalfresco,postgres',
@@ -50,14 +68,8 @@ machine_batch 'Initial setup on nodes and lb' do
 
     machine 'node2' do
       action [:ready, :setup, :converge]
-      machine_options :transport_options => {
-                          :ip_address => clusternode2,
-                          :username => username,
-                          :ssh_options => {
-                              :password => 'alfresco'
-                          }
-                      }
-      run_list %w(recipe[java-wrapper::java8] recipe[alfresco-installer::installer])
+      machine_options node2_options
+      run_list %w(recipe[java-wrapper::java8] recipe[nfs::client4] recipe[alfresco-installer::installer])
       attributes 'installer' =>
                      {'nodename' => 'node2',
                       'disable-components' => 'javaalfresco,postgres',
@@ -77,13 +89,7 @@ machine_batch 'Initial setup on nodes and lb' do
 
     machine 'LB' do
       action [:ready, :setup, :converge]
-      machine_options :transport_options => {
-                          :ip_address => loadbalancer,
-                          :username => username,
-                          :ssh_options => {
-                              :password => 'alfresco'
-                          }
-                      }
+      machine_options lb_options
       run_list %w(recipe[java-wrapper::java8] recipe[alfresco-installer::replication_server] recipe[alfresco-installer::loadbalancer] recipe[alfresco-dbwrapper::postgres] recipe[alfresco-installer::installer])
       attributes 'lb' => {
                      'ips_and_nodenames' => [
@@ -117,16 +123,21 @@ machine_batch 'Initial setup on nodes and lb' do
   end
 
   machine_batch 'replication setup' do
-    %w(node1 node2).each do |name|
-      machine name do
+      machine 'node1' do
+        machine_options node1_options
         recipe 'alfresco-installer::replication_client'
         action :converge
       end
-    end
+      machine 'node2' do
+        machine_options node2_options
+        recipe 'alfresco-installer::replication_client'
+        action :converge
+      end
   end
 
   machine 'LB' do
     action :converge
+    machine_options lb_options
     attribute 'START_SERVICES', true
     attribute %w[postgres installpostgres], false
     attribute %w[postgres createdb], false
@@ -135,11 +146,13 @@ machine_batch 'Initial setup on nodes and lb' do
 
   machine 'node1' do
     action :nothing
+    machine_options node1_options
     attribute 'START_SERVICES', true
     notifies :converge, 'machine[node2]', :immediately
   end
 
   machine 'node2' do
     action :nothing
+    machine_options node2_options
     attribute 'START_SERVICES', true
   end
