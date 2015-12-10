7d859b15287d540197cd1601e75531c6b1771072
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2015-12-10 12:03:53 +0200

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sergiu.vidrascu@ness.com'
 license '2005-2015 Alfresco Software Limited'
 description 'Installs/Configures alfresco_java'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.5'
+version '0.2.6'
 
 depends 'java'
 depends 'windows'
